848e0e1d2e4896c89db3539b9641659cac3b9fef
DELL <DELL@DESKTOP-51OVV7L>
2025-10-02 13:43:46 +0530

    commit 7

--- tnsif_java_jegan/src/inheritance/multilevelinheritance/MultilevelInheritanceDemo.java
+++ tnsif_java_jegan/src/inheritance/multilevelinheritance/MultilevelInheritanceDemo.java
@@ -11,7 +11,7 @@ public class MultilevelInheritanceDemo {
 		p1 = new Employee("Pranav", 8080807070l, new Date(1995, 05, 07), "Sales", 45000);
 		System.out.println(p1);
 
-		p1 = new LevelOneEmployee("Madhur", 9880807227l, new Date(1988, 15, 02), "Account", 85000, 200,
+		p1 = new LevelOneEmployee("Madhur", 9880807227l, new Date(1988, 1, 2), "Account", 85000, 200,
 				"Signing Authority");
 		System.out.println(p1);
 
